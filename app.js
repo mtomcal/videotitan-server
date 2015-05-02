8671a36795e516df0aef9ce36bee1ad6a036a43f
Michael Tomcal <mtomcal@cbtnuggets.com>
2015-05-02 14:23:45 -0700

    Working on importing now

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 import Hapi from 'hapi';
+import Joi from 'joi';
+import Boom from 'boom';
 
 const server = new Hapi.Server();
+
 server.connection({ port: 3000 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler(request, reply) {
-        reply('Hello, world!');
-    }
+server.register({
+  register: require('hapi-swagger'),
+  options: {
+    apiVersion: "0.1.0"
+  }
+}, (err) => {
+  if (err) {
+    server.log(['error'], 'hapi-swagger load error: ' + err);
+  } else {
+    server.log(['start'], 'hapi-swagger interface loaded');
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/{name}',
+  method: 'GET',
+  path: '/import/{username}',
+  config: {
+    tags: ['api', 'import'],
+    description: 'Import playlists for user',
     handler(request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+
+    },
+    validate: {
+      params: {
+        username: Joi.string().required()
+      }
     }
+  }
 });
 
 server.start(() => {
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
 });
