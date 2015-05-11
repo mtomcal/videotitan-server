597a9319775de4c573431ede2fa921a6fdf14405
Michael Tomcal <mtomcal@cbtnuggets.com>
2015-05-10 18:09:11 -0700

    Added working username namespace import and dynamic sources

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import Joi from 'joi';
 import Boom from 'boom';
 import config from './config/default';
 import YoutubeImport from './lib/youtube_import';
+import DB from './lib/db';
 
 const server = new Hapi.Server();
 
@@ -22,14 +23,54 @@ server.register({
 });
 
 server.route({
-  method: 'GET',
-  path: '/import/{username}',
+  method: 'POST',
+  path: '/youtube/{username}/sources',
   config: {
-    tags: ['api', 'import'],
-    description: 'Import playlists for user',
+    tags: ['api', 'youtube'],
+    description: 'Set sources for youtube',
     handler(request, reply) {
-      YoutubeImport(request.params.username, config);
-      reply({success: true, result: request.params.username});
+      DB.child(request.params.username + '/sources').set({youtube: request.payload})
+        .then(() => {
+          reply({
+            success: true,
+            result: request.payload
+          });
+        })
+        .caught((err) => {
+          reply({success: false, result: err.message});
+        });
+    },
+    validate: {
+      payload: {
+        byPlaylist: Joi.array().required(),
+        byUsername: Joi.array().required()
+      },
+      params: {
+        username: Joi.string().required()
+      }
+    }
+  }
+});
+server.route({
+  method: 'GET',
+  path: '/youtube/{username}',
+  config: {
+    tags: ['api', 'youtube'],
+    description: 'Import youtube playlists from sources',
+    handler(request, reply) {
+      YoutubeImport(request.params.username, config)
+        .then((count) => {
+          reply({
+            success: true,
+            result: {
+              "playlists": count.playlists,
+              "videos": count.videos
+            }
+          });
+        })
+        .caught((err) => {
+          reply({success: false, result: err.message});
+        });
     },
     validate: {
       params: {
